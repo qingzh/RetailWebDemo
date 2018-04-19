d8a15e4bd14151af0bd90db4be1b96ce83fc88cc
Dao Liu <daoliu@amazon.com>
2018-04-19 15:44:15 +0800

    change sensitivity

--- js/motion_detect.js
+++ js/motion_detect.js
@@ -3,12 +3,17 @@ if (window.DeviceMotionEvent) {
     var speed = 25;    // 用来判定的加速度阈值，太大了则很难触发
     var x, y, z, lastX, lastY, lastZ;
     x = y = z = lastX = lastY = lastZ = 0;
+    var index = 0;
 
-    window.addEventListener('devicemotion', function(event){
+    window.addEventListener('devicemotion', function (event) {
+        if (index++ <= 10) {
+            return;
+        }
+        index = 0;
         var acceleration = event.accelerationIncludingGravity;
         x = acceleration.x;
         y = acceleration.y;
-        $('.rwd-page-content').text('x:' + x + ' y:' + y);
+        $('.rwd-page-content').html('x:' + x + '<br>y:' + y);
         // if(Math.abs(x-lastX) > speed || Math.abs(y-lastY) > speed) {
             // 用户设备摇动了，触发响应操作
             // 此处的判断依据是用户设备的加速度大于我们设置的阈值
