44a63181e31314983f1a6a4b86286807cc5be5bc
qingzh <sjtuzhq@gmail.com>
2018-04-19 18:06:19 +0800

    Update motion_detect.js

--- js/motion_detect.js
+++ js/motion_detect.js
@@ -1,6 +1,6 @@
 
 if (window.DeviceOrientationEvent) {
-    $('#text1').html("support deviceorientation at 17:50");
+    $('#text1').html("support deviceorientation at 18:00");
     var lastAcc;    // 用来存储上一次的deviceorientation事件
     window.addEventListener('deviceorientation', function(event) {
         if ('undefined' === typeof lastAcc) { // initialize
@@ -11,11 +11,11 @@ if (window.DeviceOrientationEvent) {
         $('#text3').html("Alpha: " + event.alpha + "<br>"
         	+ "Beta: " + event.beta + "<br>"
         	+ "Gamma: " + event.gamma + "<br>");
-        var delA = Math.abs(event.alpha - lastAcc.alpha);
-        delA = Math.min(delA, Math.abs(delA-360));  // alpha轴偏转角, (0,360)
+        // alpha轴偏转角, (0,360)
+        var delA = Math.abs(360 + Math.min(event.alpha, lastAcc.alpha) - Math.max(event.alpha, lastAcc.alpha));
         var delB = Math.abs(event.beta - lastAcc.beta);    // beta轴偏转角, (-180,180)
         var delG = Math.abs(event.gamma - lastAcc.gamma);    // gamma轴偏转角, (-90, 90)
-        $('#text4').html("Alpha bias: " + delA + "<br>"
+        var msg = "Alpha bias: " + (event.alpha < lastAcc.alphadelA + "<br>"
         	+ "Beta bias: " + delB + "<br>"
         	+ "Gamma bias: " + delG + "<br>");
         if (delA > 60) {
